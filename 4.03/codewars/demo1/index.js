b3afb37bb03f9d178ba5aae3672b51a2d715a2cc
leishujing1103 <1474839743@qq.com>
2019-04-03 19:08:06 +0800

    js-question

---
+++ 4.03/codewars/demo1/index.js
@@ -0,0 +1,9 @@
+function generateHashtag(str){
+    if (str.length > 140 || str ==='')
+        return false;
+    
+    str =str.split(' ').map(word => word.charAt(0).toUpperCase() +word.slice(1)).join(' ');
+    str = '#' +str;
+    return str;
+}
+console.log(generateHashtag('hello miss dong'));
