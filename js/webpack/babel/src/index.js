fbfe57a02f0b152be2c4e4bddd5f05b8bda721ac
leishujing1103 <1474839743@qq.com>
2019-05-18 19:33:09 +0800

    babel

---
+++ js/webpack/babel/src/index.js
@@ -0,0 +1,10 @@
+const a = 1,
+b = 2;
+c = a + b;
+console.log(c);
+// [1,2,3].map(item => `${item}item`);
+
+// es6的大部分功能，其实es5也是能实现的，只不过不够优雅
+// let zmm = {name: '朱萌萌',hometown: '南昌'}
+// zmm = {...zmm, "company": '新浪'}
+// console.log(zmm);
