b26f783f9736d8046b497bd275e28e4c8dcfa59c
leishujing1103 <1474839743@qq.com>
2019-06-21 16:09:38 +0800

    vue-音乐

--- Vue/vue-music/src/App.vue
+++ Vue/vue-music/src/App.vue
@@ -17,9 +17,11 @@
     </keep-alive>
 
     <!-- play -->
+    <v-play></v-play>
+
     <!-- sidebar -->
     <v-sidebar></v-sidebar>
-
+    
   </div>
 </template>
 
@@ -27,13 +29,15 @@
 import header from '@/components/header'
 import tab from '@/components/tab'
 import sidebar from '@/components/sidebar'
+import play from '@/components/play'
 
 export default {
   name: 'App',
   components: {
     'v-header': header,
     'v-tab': tab,
-    'v-sidebar': sidebar
+    'v-sidebar': sidebar,
+    'v-play': play
   }
 }
 </script>
