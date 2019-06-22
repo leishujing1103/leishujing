36dd67f31c0fd762bd55f836c5dd6b19193dbbcc
leishujing1103 <1474839743@qq.com>
2019-06-22 22:58:41 +0800

    express

---
+++ js/node/express-demo/app.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded ({
+    extended: true
+}));
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World</h1>');
+})
+router.get('/:name', function(req, res) {
+    res.send('<h1>Hello' + req.params.name + '</h1>')
+})
+
+router.post('/', function(req, res) {
+    var name = req.body.name;
+    res.json({message: 'hello' + name});
+})
+var port = process.env.PORT || 8080;
+
+app.use('/home', router)
+app.listen(port);
+console.log('magic happens on port' + port);
