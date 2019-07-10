b28f96a7e180979a851a499512ffdb9d59af7ee4
leishujing1103 <1474839743@qq.com>
2019-07-10 14:38:54 +0800

    react-dynamicField

---
+++ react/react-dynamic-filed/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DynamicField from './DynamicField'
+
+
+function App() {
+  return (
+    <div>
+      <DynamicField />
+    </div>
+  );
+}
+
+export default App;
